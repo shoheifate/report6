1f66b12d7a0eff0e900425e6fe0edd5c0ca66856
Tamashiro syohei <e175752@ie.u-ryukyu.ac.jp>
2018-01-14 17:10:54 +0900

    3rd commit

--- src/main/java/jp/ac/uryukyu/ie/e175752/Kuku.java
+++ src/main/java/jp/ac/uryukyu/ie/e175752/Kuku.java
@@ -3,24 +3,7 @@ import java.util.Scanner;
 
 public class Kuku {
 //九九の表を出力できるコードを有効活用して、九九の段を学べるコードを書きたい。
-       /*public static void main(String[] args){
-            System.out.println("九九の表（ひょう）");
-            System.out.println("この表を参考（さんこう）にして、九九の問題（もんだい）を解（と）いてみよう！！");
 
-            for (int i = 1; i <= 9; i++) {
-                for (int j = 1; j <= 9; j++) {
-                    System.out.print(i * j + " ");
-                }
-                System.out.println();
-            }}*/
-            //掛け算の表を出力して、よりわかりやすく問題を解かせる。
-    //double rand = Math.random() * 9 + 1;
-    //double rand_2 = Math.random() * 9 + 1;
-    //int number = (int) rand;
-    //int number_2 = (int) rand_2;
-    //System.out.println(number * number_2);//答え
-    //int ns = (int) rand * (int) rand_2;
-    //System.out.println(ns);
 
        public void SetKuku() {
            double rand = Math.random() * 9 + 1;
@@ -31,9 +14,9 @@ public class Kuku {
 
            int number = (int) rand;
            int number_2 = (int) rand_2;
-           //System.out.println(number * number_2);//答え
+           //System.out.println(number * number_2);//答えを可視化するときに有効にする。
            int ns = (int) rand * (int) rand_2;
-           //System.out.println(ns);
+
 
            Scanner in = new Scanner(System.in);
            int k = 0;
@@ -60,9 +43,9 @@ public class Kuku {
 
             int number_1 = (int) rand_3;
             int number_3 = (int) rand_4;
-            //System.out.println(number * number_2);//答え
+            //System.out.println(number * number_2);//答えを可視化させるときに有効にする。
             int nj = (int) rand_3 * (int) rand_4;
-            //System.out.println(ns);
+
 
             Scanner in = new Scanner(System.in);
             int j =0;
---
+++ src/test/java/jp/ac/uryukyu/ie/e175752/KukuTest.java
@@ -0,0 +1,14 @@
+package jp.ac.uryukyu.ie.e175752;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KukuTest {
+    @Test
+    void setKuku() {
+        Kuku kuku = new Kuku();
+        kuku.SetKuku();
+    }
+
+}
